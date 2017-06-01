bd4819f95cc8c501a132dd6d045557852fcd472b
Jason Bruderer <Lastrellik@gmail.com>
2017-05-31 23:12:27 -0600

    Tests Passing

--- WSBackupReportTests/SettingsTest.cs
+++ WSBackupReportTests/SettingsTest.cs
@@ -1,14 +1,71 @@
 ﻿using System;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
 using WSBackupReport;
+using System.IO;
+using NUnit.Framework;
 
 namespace WSBackupReportTests {
-	[TestClass]
+
+	[TestFixture]
 	public class SettingsTest {
-		[TestMethod]
-		public void TestMethod1() {
-			Settings settings = new WSBackupReport.Settings();
-			Assert.AreEqual(1, 1);
+
+		[SetUp]
+		protected void SetUp() {
+			//File.Delete("testSettings.ini");
+		}
+
+		[TearDown]
+		public void TearDown() {
+			//File.Delete("testSettings.ini");
+		}
+
+
+		[Test]
+		public void TestConstructor_FileDoesNotExist() {
+			string fileName = "testSettings.ini";
+			File.Delete(fileName);
+			bool doesFileExistBefore = File.Exists(fileName);
+			Settings settings = new WSBackupReport.Settings(fileName);
+			Assert.AreNotEqual(doesFileExistBefore, File.Exists(fileName));
+		}
+
+		[Test]
+		public void TestConstructor_FileDoesExist() {
+			string fileName = "secondTestSettings.ini";
+			var theFile = File.Create(fileName);
+			theFile.Close();
+			bool doesFileExist = File.Exists(fileName);
+			Settings settings = new WSBackupReport.Settings(fileName);
+			Assert.AreEqual(doesFileExist, File.Exists(fileName));
+		}
+
+		[Test]
+		public void TestConstructor_InvalidName() {
+			Settings settings;
+			Assert.Throws<ArgumentException>(() => settings = new WSBackupReport.Settings("testSettings"));
+		}
+
+		[Test]
+		public void TestConstructor_NullInput() {
+			Settings settings;
+			Assert.Throws<NullReferenceException>(() => settings = new WSBackupReport.Settings(null));
+		}
+
+		[Test]
+		public void TestSetAttribute() {
+			String fileName = "TestSettings1.ini";
+			File.Delete(fileName);
+			Settings settings = new Settings(fileName);
+			settings.setAttribute("ToEmailAddress", "test@email.com");
+			Assert.AreEqual("test@email.com" , settings.getAttribute("ToEmailAddress"));
+		}
+
+		[Test]
+		public void TestGetAttribute() {
+			String fileName = "TestSettings2.ini";
+			File.Delete(fileName);
+			Settings settings = new Settings(fileName);
+			settings.setAttribute("ToEmailAddress", "test@email.com");
+			Assert.AreEqual("test@email.com", settings.getAttribute("ToEmailAddress"));
 		}
 	}
 }
