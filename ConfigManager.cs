c8af96bc2bf03d58dfddca04e18bb831cf10b349
Jason Bruderer <Lastrellik@gmail.com>
2017-06-01 23:41:20 -0600

    Refactor to ConfigManager

---
+++ ConfigManager.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace WSBackupReport {
+
+	public class ConfigManager {
+		private const string CONFIG_FILE_NAME = "WSBackupReport.ini";
+		private bool isFirstRun = false;
+		private Dictionary<string, string> attributeTable;
+
+
+		public static void Main(string[] args) {
+		}
+
+		public ConfigManager() {
+			if (!File.Exists(CONFIG_FILE_NAME)) initializeApp();
+			parseConfigFile();
+		}
+
+		public bool isInitialRun() {
+			return isFirstRun;
+		}
+
+		private void initializeApp() {
+			isFirstRun = true;
+			File.Create(CONFIG_FILE_NAME);
+		}
+
+		private void parseConfigFile() {
+			if (isFirstRun) return;
+		}
+
+		public void setAttribute(String key, String value) {
+			attributeTable.Add(key.ToUpper(), value);
+		}
+
+		public string getAttribute(string key) {
+			return attributeTable[key.ToUpper()];
+		}
+	}
+}
---
+++ WSBackupReportTests/ConfigManagerTest.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using WSBackupReport;
+using System.IO;
+using NUnit.Framework;
+
+namespace WSBackupReportTests {
+
+	[TestFixture]
+	public class ConfigManagerTest {
+		private const string CONFIG_FILE_NAME = "WSBackupReport.ini";
+		private ConfigManager manager;
+
+		[SetUp]
+		protected void SetUp() {
+			collectGarbage();
+			File.Delete(CONFIG_FILE_NAME);
+			manager = new ConfigManager();
+		}
+
+		[TearDown]
+		public void TearDown() {
+			manager = null;
+			collectGarbage();
+			File.Delete(CONFIG_FILE_NAME);
+		}
+
+
+		[Test]
+		public void TestConstructor() {
+			Assert.IsTrue(File.Exists(CONFIG_FILE_NAME));
+		}
+
+		[Test]
+		public void TestConstructor_NotFirstTime() {
+			manager = null;
+			collectGarbage();
+			if (!File.Exists(CONFIG_FILE_NAME)) File.Create(CONFIG_FILE_NAME);
+			manager = new ConfigManager();
+			Assert.IsFalse(manager.isInitialRun());
+		}
+
+		[Test]
+		public void TestIsInitialRun() {
+			manager = null;
+			collectGarbage();
+			File.Delete(CONFIG_FILE_NAME);
+			manager = new ConfigManager();
+			Assert.IsTrue(manager.isInitialRun());
+		}
+
+		[Test]
+		public void TestSetAttribute() {
+			manager.setAttribute("To Address", "to@emailaddress.com");
+			Assert.AreEqual("to@emailaddress.com", manager.getAttribute("To Address"));
+		}
+
+		[Test]
+		public void TestGetAttribute() {
+			manager.setAttribute("To Address", "to@emailaddress.com");
+			Assert.AreEqual("to@emailaddress.com", manager.getAttribute("To Address"));
+		}
+
+		private void collectGarbage() {
+			System.GC.Collect();
+			System.GC.WaitForPendingFinalizers();
+		}
+		
+	}
+}
